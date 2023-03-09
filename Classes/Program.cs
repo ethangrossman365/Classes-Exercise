14ff0a764a9355ccf5196307ee85e9c9b3195f36
ethan grossman <ethangrossman3@gmail.com>
2023-03-09 14:29:04 -0600

    finished exercise 1 for classes

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Dodge";
+            myCar.Model = "Challenger";
+            myCar.Year = "2022";
+
+            Console.WriteLine($"{myCar.Make} {myCar.Model} {myCar.Year}");
+
+           
         }
     }
 }
